978f03ecc4762b8f434e96252ebad273b799f21f
Pinho <rafaelpinhotsilva@gmail.com>
2026-05-17 17:38:29 +0100

    Completed run.sh

--- run.sh
+++ run.sh
@@ -15,10 +15,19 @@ elif [ "$#" -eq 4 ] && [ "$1" = "-b" ]; then
     cmake .. &&
     make &&
     cd ../.. &&
-    ./code/cmake-build/register_alloc -b "dataset/$2" "dataset/$3" "outputs/$4"
+    ./code/cmake-build/register_alloc -b "$2" "$3" "$4"
+elif [ "$#" -eq 5 ] && [ "$1" = "-b" ]; then
+    mkdir -p code/cmake-build &&
+    mkdir -p outputs &&
+    cd code/cmake-build &&
+    cmake .. &&
+    make &&
+    cd ../.. &&
+    ./code/cmake-build/register_alloc -b "$2" "$3" "$4" "$5"
 else
     echo "Usage:"
     echo "  $0"
-    echo "  $0 -b <ranges_path> <registers_path> <output_path>"
+    echo "  $0 -b <ranges_full_path> <registers_full_path> <output_full_path>"
+    echo "  $0 -b <dataset_type> <ranges_filename> <registers_filename> <output_filename>"
     exit 1
 fi
